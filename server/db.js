bc03aec773d70d6d921b276f2f198209767890de
GlebTheProgrammer <55713244+GlebTheProgrammer@users.noreply.github.com>
2023-03-28 18:59:34 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,8 +2,8 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'Master95%',
+    user: 'Your user',
+    password: 'Your password',
     host: 'localhost',
     port: 5432,
     database: 'todoapp'
